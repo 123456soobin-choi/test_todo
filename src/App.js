3ea41c3045d89f0ebf3f2565b6be463e7c590fcd
최수빈 <choesubin@choesubin-ui-MacBookAir.local>
2022-12-01 12:26:35 +0900

    add

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [con, setCon] = useState("");
-  const [toDos, setToDos] = useState([]);
+  const [con, setCon] = useState(""); //새로운 값을 담을 con 초기값 선언
+  const [toDos, setToDos] = useState([]); //con들을 담을 배열 초기값 선언
 
   const onChangeHandler = (event) => {
-    setCon(event.target.value);
+    setCon(event.target.value); //input에 값 입력했을 때 value값 가져오기
   };
 
   const onCreateHandler = (event) => {
     if (con === "") {
-      return;
+      return; //아무것도 입력하지 않으면 함수 실행시키지 않음
     }
-    setToDos((currentArray) => [con, ...currentArray]);
-    setCon("");
+    setToDos([...toDos, con]); //create한 con 넣은 배열로 갈아끼우기
+    // setToDos((currentArray) => [con, ...currentArray]);
+    setCon(""); //con 초기화
   };
   console.log(toDos);
 
@@ -23,7 +24,7 @@ function App() {
       <header>
         <div className="add-form">
           <input
-            className="con"
+            className="content"
             onChange={onChangeHandler}
             type="text"
             value={con}
@@ -42,5 +43,6 @@ function App() {
     </div>
   );
 }
+/*toDos배열에서 con을 하나씩 각각 불러와서 container에 담아주기 */
 
 export default App;
